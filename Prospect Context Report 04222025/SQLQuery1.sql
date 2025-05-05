ed1c1b4db7470a5cf6db9ab8d3274be7ac4bc01f
Shreyas Sridhar <sridhs7@mcmaster.ca>
2025-05-05 14:54:15 -0400

    initial commit

---
+++ Prospect Context Report 04222025/SQLQuery1.sql
@@ -0,0 +1,11 @@
+﻿With Personal_Details AS(
+select top 1000 
+contactId AS personId,
+elcn_PrimaryID AS ID_NUMBER, 
+elcn_SortName AS PREF_NAME_SORT,
+fullname AS FULL_NAME
+from ContactBase cb)
+select pd.*, pre.elcn_person2idname AS SPOUSE_NAME from Personal_Details pd
+left join  Filteredelcn_personalrelationship pre 
+on pd.personId = pre.elcn_person1id
+where pre.elcn_relationshiptype1idname = 'Spouse'
