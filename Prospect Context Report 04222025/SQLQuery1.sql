424f88d2b63fd3f1c6703c9b4582432bd8b21da5
Shreyas Sridhar <sridhs7@mcmaster.ca>
2025-05-05 17:27:36 -0400

    business info

--- Prospect Context Report 04222025/SQLQuery1.sql
+++ Prospect Context Report 04222025/SQLQuery1.sql
@@ -1,9 +1,11 @@
-﻿With Personal_Details AS(
+﻿-- Personal Details
+With Personal_Details AS(
 select 
 contactId AS personId,
 elcn_PrimaryID AS ID_NUMBER, 
 elcn_SortName AS PREF_NAME_SORT,
-fullname AS FULL_NAME
+fullname AS FULL_NAME,
+EMailAddress1 AS EMAIL_ADDRESS
 from ContactBase cb)
 select pd.*, pre.elcn_person2idname AS SPOUSE_GUID, pre.elcn_person2idname AS SPOUSE_NAME 
 from Personal_Details pd
@@ -11,3 +13,21 @@ left join  Filteredelcn_personalrelationship pre
 on pd.personId = pre.elcn_person1id
 where (pre.elcn_relationshiptype1idname = 'Spouse' or pre.elcn_relationshiptype1idname is null)
 and pd.personId = '0CF255E9-B005-4794-AD2F-382F371D6719'
+
+-- TO DO: Get Spouse Advance ID and Constituent
+
+--Education
+select  
+fe.elcn_degreeyear AS DEG_YEAR,
+fe.elcn_degreeidname AS DEG_CODE_DESC,
+fe.elcn_collegeidname AS DEG_SCHOOL_CODE_DESC,
+fe.elcn_departmentidname AS DEG_MAJOR1_DESC
+from Filteredelcn_education fe
+where elcn_personid = '0CF255E9-B005-4794-AD2F-382F371D6719'
+order by fe.elcn_degreeyear 
+
+--Business Info
+select top 1000 * from Filteredelcn_businessrelationship fbr
+where elcn_personid = '0CF255E9-B005-4794-AD2F-382F371D6719'
+and fbr.elcn_businessrelationshipstatusidname = 'Active'
+and fbr.elcn_businessrelationshiptypeidname = 'Primary Employer'
